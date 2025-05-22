9c79d9ffe2e4b392196dce8a9ce34af19e6b7148
Clever Cottonmouth <ab6580203@gmail.com>
2025-05-22 20:38:16 +0530

    update 4 files and create 1 file

--- src/main.js
+++ src/main.js
@@ -7,10 +7,19 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import App from './App.vue'
 import router from './router/routes'
-
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { useThemeStore } from './stores/themeStore'
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+
+const themeStore = useThemeStore()
+if (themeStore.theme) {
+  document.body.setAttribute('data-bs-theme', themeStore.theme)
+}
 
 app.mount('#app')
