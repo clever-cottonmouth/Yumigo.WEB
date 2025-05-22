75437955444c8ca6703efff9cb4e40825b8de93f
Clever Cottonmouth <ab6580203@gmail.com>
2025-05-22 16:20:11 +0530

    clean up

--- src/main.js
+++ src/main.js
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import router from './router/routes'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-=======
-
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import ContactUs from './components/ContactUs.vue'
-
-const app = createApp(App)
-
-app.component('contact-us', ContactUs)
->>>>>>> 1a48234752524a9f5ce3e6aa1e71dc36365dfc70
 
 app.mount('#app')
